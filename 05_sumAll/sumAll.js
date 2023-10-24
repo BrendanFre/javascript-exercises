61a2b6809023c71da7bf60f2a4a27570dd3ae3c2
Brendan Freeman <80400320+BrendanFre@users.noreply.github.com>
2023-10-24 13:52:54 +0000

    Compeleted sumall

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function (firstNumber, secondNumber) {
+    let smallNumber
+    let largeNumber
 
+    if (firstNumber < 0 || secondNumber < 0) {
+        return 'ERROR'
+    } else if (typeof(firstNumber) != "number"|| typeof(secondNumber) != "number") { return 'ERROR' } else if (firstNumber > secondNumber) {
+        smallNumber = secondNumber
+        largeNumber = firstNumber
+    } else {
+        smallNumber = firstNumber
+        largeNumber = secondNumber
+    }
+
+    let loopIndex
+    let sum = parseInt(smallNumber)
+
+
+    for (let i = smallNumber; i < largeNumber; i++) {
+        sum = sum + (i + 1)
+
+    }
+    return sum
 };
 
 // Do not edit below this line
